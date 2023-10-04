3ebf39decd8d3a778c5389eda331a8bf64938155
Meek Msaki <meek10x@gmail.com>
2023-10-03 21:34:58 -0500

    chore: update remappings

--- src/TheMoneyBoy$.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Base64.sol";
-
-contract TheMoneyBoys is ERC721, ownable {}
